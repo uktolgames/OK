e424c82bca5550a47cb14bde5cfa08a62d5df70f
Lars Kwant <Lars@iMac-van-Lars.local>
2015-05-08 11:25:33 +0200

    Initial Commit

---
+++ OK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OK
+//
+//  Created by Lars Kwant on 08-05-15.
+//  Copyright (c) 2015 Uktol Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OK
+//
+//  Created by Lars Kwant on 08-05-15.
+//  Copyright (c) 2015 Uktol Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
